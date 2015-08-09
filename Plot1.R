83d4dad703ffc096b59b894ab707e8c572c7e87a
jasonabr <jasonabr@gmail.com>
2015-08-09 00:16:44 -0700

    First plots

---
+++ Plot1.R
@@ -0,0 +1,6 @@
+power <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+power.sub <- subset(power, Date=="1/2/2007" | Date=="2/2/2007")
+power.sub$Global_active_power <- as.numeric(power.sub$Global_active_power)
+hist(power.sub$Global_active_power, main="Global Active Power", xlab ="Global Active Power (kilowatts)", ylab="Frequency", col="red")
+dev.copy(png, file="Plot1.png", width=480, height=480)
+dev.off()
---
+++ Plot2.R
@@ -0,0 +1,8 @@
+power <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+power.sub <- subset(power, Date=="1/2/2007" | Date=="2/2/2007", select=c("Date","Time", "Global_active_power"))
+GlobalActivePower <- as.numeric(power.sub$Global_active_power)
+DT <- with(power.sub, paste(Date, Time))
+DT <- strptime(DT, "%d/%m/%Y %H:%M:%S")
+plot(DT, GlobalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.copy(png, file="Plot2.png", width=480, height=480)
+dev.off()
